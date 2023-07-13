e25cee274ef98b391100cffe999cce838d846cc8
Justin Yang <justinyang@justins-mbp.home>
2023-07-13 16:50:19 -0400

    added main components

--- homeflow/api/urls.py
+++ homeflow/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, main
 
 urlpatterns = [
     path('home', RoomView.as_view()),
+    path('hello', main),
 ]
--- homeflow/api/views.py
+++ homeflow/api/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import RoomSerializer
 from .models import Room
+
+def main(request):
+    return HttpResponse("hello!!") 
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
